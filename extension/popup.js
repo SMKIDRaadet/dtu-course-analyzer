4ed1100ed2d627c0e596cdc6d715b46738756d6d
Oskar Soerensen <ons@onit.dk>
2016-11-24 17:10:40 +0100

    Implemented JSON parser for scraped data and extension skeleton

---
+++ extension/popup.js
@@ -0,0 +1,9 @@
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('status').textContent = "waaaddduuuuupppp 420";
+  
+});
