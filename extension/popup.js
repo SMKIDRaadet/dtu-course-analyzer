d4816cf759c547d1b71e266ea2544643955a8ce8
Oskar Nedergaard Sørensen <ons@onit.dk>
2016-11-28 11:56:32 +0100

    Implemented extension

--- extension/popup.js
+++
@@ -1,9 +0,0 @@
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('status').textContent = "waaaddduuuuupppp 420";
-  
-});
