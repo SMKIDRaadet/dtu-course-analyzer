69680323562c6902b0f0febe34dcdcfdbf45541a
Oskar Soerensen <soerensen.oskar@gmail.com>
2018-12-03 21:11:54 +0100

    Ported the extensino to Firefox

--- firefox_extension/background.js
+++ firefox_extension/background.js
@@ -1,27 +1,19 @@
-chrome.extension.onRequest.addListener(function(request, sender) {
+browser.runtime.onMessage.addListener(function(request, sender) {
 	course=request.getInfo
 	rtab=sender.tab.id
-	//console.log(rtab + ":'" + course + "'");
 	
 	var resp={};
 	if(course in data){
 		resp[course]=data[course]
 	} else{
-		//console.log("No data for this course")
 		resp[course]=false
 	}
 	
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(rtab, resp);
+	browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		browser.tabs.sendMessage(rtab, resp);
 	});
 });
-
 	
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.create({ url: chrome.extension.getURL('db.html') });
+browser.browserAction.onClicked.addListener(function(tab) {
+	browser.tabs.create({ url: browser.extension.getURL('db.html') });
 });
-function getColor(value){
-    //value from 0 to 1
-    var hue=((1-value)*120).toString(10);
-    return ["hsl(",hue,",100%,50%)"].join("");
-}
