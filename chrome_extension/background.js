86e253dbb0e8397f8b464e32e2cb43c02e588a65
Hans Harck Tønning <developer@hansit.dk>
2022-12-17 13:26:27 +0100

    Merges background.js

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,14 +1,14 @@
 chrome.extension.onRequest.addListener(function(request, sender) {
 	course=request.getInfo
 	rtab=sender.tab.id
-	//console.log(rtab + ":'" + course + "'");
 	
 	var resp={};
 	if(course in data){
-		resp[course]=data[course]
+		console.log(data[course]);
+		resp[course]=data[course];
 	} else{
-		//console.log("No data for this course")
-		resp[course]=false
+		console.log("No data for this course");
+		resp[course]=false;
 	}
 	
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -20,8 +20,3 @@ chrome.extension.onRequest.addListener(function(request, sender) {
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.create({ url: chrome.extension.getURL('db.html') });
 });
-function getColor(value){
-    //value from 0 to 1
-    var hue=((1-value)*120).toString(10);
-    return ["hsl(",hue,",100%,50%)"].join("");
-}
